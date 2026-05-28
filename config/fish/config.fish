a3283b430fd9c51ea1f7f24b98196a0b95b737c4
Jaeyong Sung <jaeyong0201@gmail.com>
2026-05-28 19:59:27 +0900

    auto(dotfiles): Jaeyongs-MacBook-Pro 2026-05-28T19:59:21+0900

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,2 +1,6 @@
 # Shared fish setup is split across conf.d/*.fish.
 # Private overrides live outside the repo under ~/.config/local.
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
